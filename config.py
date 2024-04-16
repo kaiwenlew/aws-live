ca4e559bfaa0264a51e01be1af7cbcaafd4b8830
kaiwenlew <162236873+kaiwenlew@users.noreply.github.com>
2024-04-16 15:47:27 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.c7kw0qka2opj.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "lewkaiwen-employee"
+customregion = "us-east-1"
+
